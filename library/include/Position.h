793b78288f5c759c3b6a5e0263b6632225065fc3
jose <jar2333@columbia.edu>
2022-11-27 03:19:25 +0000

    Solver almost done besides main algorithm

--- library/include/Position.h
+++ library/include/Position.h
@@ -4,3 +4,7 @@ struct Position {
     int x;
     int y;
 };
+
+inline bool operator <(const Position& lhs, const Position& rhs){
+    return lhs.x >= rhs.x ? lhs.y < rhs.y : true;
+}
--- library/include/Solver.h
+++ library/include/Solver.h
@@ -5,6 +5,7 @@
 #include <Position.h>
 
 #include <map>
+#include <set>
 #include <vector>
 #include <list>
 #include <functional>
@@ -19,7 +20,7 @@ public:
 
   typedef Tile T;
 
-  typedef typename int TileKey;
+  typedef int TileKey;
 
   typedef std::function<void(const T&, Position)> CollapseCallbackT;
   typedef std::function<void(const std::vector<T>&, Position)> PropagateCallbackT;  
@@ -28,14 +29,14 @@ public:
   typedef typename std::list<PropagateCallbackT>::iterator PropagateCallbackCookie;
 
   //the main way of using the library, the int keys are auto-generated from vector as the indeces
-  Solver(const std::vector<T>& tiles, int seed=0) {
+  Solver(const std::vector<T>& tiles, int seed=0): seed(seed) {
     for (TileKey k = 0; k < tiles.size(); k++) {
       this->tiles[k] = tiles[k];
     }
   }
 
   //similar, but keys are provided explicitly for each tile
-  Solver(const std::map<TileKey, T>& tiles, int seed=0) {
+  Solver(const std::map<TileKey, T>& tiles, int seed=0): seed(seed) {
     this->tiles = tiles;
   }
 
@@ -57,16 +58,41 @@ public:
 
   /* Adjacency constraints */
   //set the constraints using this specified interface
-  void addAdjacencyConstraint(TileKey t, Direction d, TileKey neighbor);
-  void removeAdjacencyConstraint(TileKey t, Direction d, TileKey neighbor);
+  void addAdjacencyConstraint(TileKey t, Direction d, TileKey neighbor) {
+    adjacency_constraints[std::make_pair(t, d)].insert(neighbor);
+  }
+
+  void removeAdjacencyConstraint(TileKey t, Direction d, TileKey neighbor) {
+    adjacency_constraints[std::make_pair(t, d)].erase(neighbor);
+  }
 
   //using initializer list
-  void addAdjacencyConstraints(TileKey t, Direction d, std::initializer_list<TileKey> neighbors);
-  void removeAdjacencyConstraints(TileKey t, Direction d, std::initializer_list<TileKey> neighbors);
+  void addAdjacencyConstraints(TileKey t, Direction d, std::initializer_list<TileKey> neighbors) {
+    auto p = std::make_pair(t, d);
+    for (auto n : neighbors) {
+      adjacency_constraints[p].insert(n);
+    }
+  }
+
+  void removeAdjacencyConstraints(TileKey t, Direction d, std::initializer_list<TileKey> neighbors) {
+    auto p = std::make_pair(t, d);
+    for (auto n : neighbors) {
+      adjacency_constraints[p].erase(n);
+    }
+  }
 
   /* Initial constraints */
-  void setInitialConstraint(Position p, TileKey possibility);
-  void setInitialConstraint(Position p, std::initializer_list<TileKey> possibilities);
+  void setInitialConstraint(Position p, TileKey possibility) {
+    initial_constraints[p].clear();
+    initial_constraints[p].push_back(possibility);
+  }
+
+  void setInitialConstraint(Position p, std::initializer_list<TileKey> possibilities) {
+    initial_constraints[p].clear(); 
+    for (auto k : possibilities) {
+      initial_constraints[p].push_back(k);
+    }
+  }
   
   /*
    CALLBACK INTERFACE/API
@@ -74,22 +100,22 @@ public:
  
   //To-do: returns a receipt object to deregister when out of scope?
   //API: calls registered function with tile in collapsed grid slot
-  CollapseCallbackCookie registerCollapse(std::function<void(const T&, Position)> callback) {
+  CollapseCallbackCookie registerCollapse(CollapseCallbackT callback) {
     return collapse_callbacks.insert(collapse_callbacks.begin(), callback);
   }
 
   void deregisterCollapse(CollapseCallbackCookie cookie) {
-    return collapse_callbacks.erase(cookie);
+    collapse_callbacks.erase(cookie);
   }
 
 
   //API: calls registered function with possible tiles in collapsed grid slot
-  PropagateCallbackCookie registerPropagate(std::function<void(const std::vector<T>&, Position)> callback) {
-    return propagate_callbacks.insert(collapse_callbacks.begin(), callback);
+  PropagateCallbackCookie registerPropagate(PropagateCallbackT callback) {
+    return propagate_callbacks.insert(propagate_callbacks.begin(), callback);
   }
 
   void deregisterCollapse(PropagateCallbackCookie cookie) {
-    return propagate_callbacks.erase(cookie);
+    propagate_callbacks.erase(cookie);
   }
 
 
@@ -107,12 +133,16 @@ public:
 
 
 private:
+  void initializeGrid(int N);
+
   int seed;
 
   std::map<TileKey, T> tiles;
   //  std::function<TileKey(std::vector&<TileKey>)> heuristic;
 
-  std::unordered_map<std::pair<TileKey, Direction>, std::unordered_set<TileKey>> adjacency_constraints;
+  std::map<std::pair<TileKey, Direction>, std::unordered_set<TileKey>> adjacency_constraints;
+
+  std::map<Position, std::vector<TileKey>> initial_constraints;
 
   std::list<CollapseCallbackT> collapse_callbacks;
   std::list<PropagateCallbackT> propagate_callbacks;
